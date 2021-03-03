379e1537f33630396b4441d969978f440c4fc77a
Lukis02 <74176312+Lukis02@users.noreply.github.com>
2021-03-03 13:47:18 +0100

    Add files via upload

--- hotel/src/hotel.java
+++ hotel/src/hotel.java
@@ -1,23 +1,45 @@
+import javax.swing.*;
+import java.awt.*;
 import java.sql.SQLOutput;
+import java.util.Date;
 import java.util.Scanner;
 
-public class hotel {
+import datechooser.beans.DateChooserCombo;
+import datechooser.beans.DateChooserDialog;
+
+public class hotel extends JFrame {
+    private boolean reservas;
+    public static Scanner sc = new Scanner(System.in);
+    public static String clientes = sc.nextLine();
+    private static JCheckBox doble, suite;
+    private static JButton aceptar, reiniciar;
+    private static DateChooserCombo Entrada;
+    private static DateChooserCombo Salida;
+
+    public hotel() {
+        super ("Hotel el pepe");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        JPanel p1 = new JPanel();
+        p1.setLayout(new GridLayout(3,2,50,50));
+        Entrada = new DateChooserCombo();
+        Salida = new DateChooserCombo();
+        p1.add(Entrada);
+        p1.add(Salida);
+        JPanel p2 = new JPanel();
+        p2.setLayout(new GridLayout(1,2));
+        JPanel p3 = new JPanel();
+        p3.setLayout(new GridLayout(4,2));
 
 
+
+        }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduzca el nombre del nuevo cliente");
-        String Cliente = sc.nextLine();
-        System.out.println("Introduzca el número de habitaciones");
-        Integer numhab = sc.nextInt();
-        System.out.println("Introduzca el numero de dias");
-        Integer numdias=sc.nextInt();
-        //*ahora estoy pensando en hacer un boolean para la habitación para saber si está ocupada, tras esto meto un if para comprobar si es true o false, si es false,
-        // lo pongo en true para el cliente, de lo contrario nose
+
+    }
 
 
 
-
-
-    }}
+    }
